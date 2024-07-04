43ed5b0a6be04329b284d0c5c0ea0ee0ed7da66e
hemanthkumar314 <bhemanthkumar314@gmail.com>
2024-07-04 15:36:21 +0530

    Updated1

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from . import db
 from .models import User
 from .models import Workout
+from .models import Target
 from flask import Blueprint, render_template, redirect, url_for, request, flash, abort
 from flask_login import current_user, login_required
 
@@ -20,7 +21,7 @@ def home():
 def profile():
     return render_template('profile.html', name=current_user.name)
 
-
+#all workouts
 @main.route("/all")
 @login_required
 def user_workouts():
@@ -28,12 +29,30 @@ def user_workouts():
     workouts = user.workouts  # Workout.query.filter_by(author=user).order_by(Workout.date_posted.desc())
     return render_template('all_workouts.html', workouts=workouts, user=user)
 
+#new target
+@main.route("/newtarget")
+@login_required
+def new_target():
+    return render_template('target.html')
 
+@main.route("/newtarget", methods=['POST'])
+@login_required
+def new_target_post():
+    workout=request.form.get('workout')
+    count = request.form.get('count')
+    date = request.form.get('date')
+    print(workout,count, date)
+    workout = Target(workout=workout,count=count,target=date, author=current_user)
+    db.session.add(workout)
+    db.session.commit()
+    flash('Your workout has been added!')
+    return redirect(url_for('main.home'))
+
+#new workout
 @main.route("/new")
 @login_required
 def new_workout():
     return render_template('create_workout.html')
-
 
 @main.route("/new", methods=['POST'])
 @login_required
@@ -46,9 +65,9 @@ def new_workout_post():
     db.session.add(workout)
     db.session.commit()
     flash('Your workout has been added!')
-    return redirect(url_for('main.index'))
+    return redirect(url_for('main.home'))
 
-
+#update workout
 @main.route("/workout/<int:workout_id>/update", methods=['GET', 'POST'])
 @login_required
 def update_workout(workout_id):
@@ -58,17 +77,17 @@ def update_workout(workout_id):
         workout.count = request.form['count']
         workout.comment = request.form['comment']
         db.session.commit()
-        flash('Your post has been updated!')
+        flash('Your workout has been updated!')
         return redirect(url_for('main.user_workouts'))
 
     return render_template('update_workout.html', workout=workout)
 
-
+#delete workout
 @main.route("/workout/<int:workout_id>/delete", methods=['GET', 'POST'])
 @login_required
 def delete_workout(workout_id):
     workout = Workout.query.get_or_404(workout_id)
     db.session.delete(workout)
     db.session.commit()
-    flash('Your post has been deleted!')
+    flash('Your workout has been deleted!')
     return redirect(url_for('main.user_workouts'))
--- app/models.py
+++ app/models.py
@@ -2,7 +2,6 @@ from flask_login import UserMixin
 from datetime import datetime
 from . import db
 
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)  
     email = db.Column(db.String(100), unique=True)
@@ -17,4 +16,12 @@ class Workout(db.Model):
     count = db.Column(db.Integer, nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     comment = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class Target(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    workout = db.Column(db.Text, nullable=False)
+    count = db.Column(db.Integer, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    target = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
